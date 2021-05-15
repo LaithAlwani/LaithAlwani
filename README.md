9e8166cc098ea9b9bc787efde3e020d2bc49bf25
Laith Alwani <laithalwani@gmail.com>
2021-05-14 23:10:49 -0400

    added readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+<a>
+ <img align="center" src="https://github-readme-stats.vercel.app/api?username=LaithAlwani&theme=dark"
+</a>
+
+<a >
+ <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=LaithAlwani&theme=dark"
+</a>
+ 
+ ![willianrod's wakatime stats](https://github-readme-stats.vercel.app/api/wakatime?username=laithA)
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
+  
