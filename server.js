9b5ddecdaa8e1417802a5c4e9231c9c570c007a6
Laith Alwani <laithalwani@gmail.com>
2021-05-20 22:09:26 -0400

    added express http to https

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const redirect = require("express-http-to-https").redirectToHTTPS
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(redirect());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
