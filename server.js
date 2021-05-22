d6bcc83ac20308bbf230f4688792d54a4b82e86c
Laith Alwani <laithalwani@gmail.com>
2021-05-22 12:30:28 -0400

    changed varbiable name

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const redirect = require("express-http-to-https").redirectToHTTPS
+const redirectToHTTPS = require("express-http-to-https").redirectToHTTPS
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(redirect());
+app.use(redirectToHTTPS());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
