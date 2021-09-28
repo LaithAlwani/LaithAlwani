18537891e63e535bc1d01599596eca1c3ad6b336
Laith Alwani <laithalwani@gmail.com>
2021-09-28 16:21:57 -0400

    fixed mobiel view issue

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Test() {
   return (
-    <h1 className="intro  animate__animated animate__zoomOut animate__delay-1s animate__slower">
+    <h1 className="intro align-content animate__animated animate__zoomOut animate__delay-1s animate__slower">
       LAITH ALWANI
     </h1>
   );
