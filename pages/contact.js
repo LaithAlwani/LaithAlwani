36e3ae7aecc8dd0f8239bdd485d48406f8ea4d38
Laith Alwani <laithalwani@gmail.com>
2022-01-11 18:15:05 -0500

    rough darft of contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import styles from "../styles/contact.module.css";
 
 export default function contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
   return (
-    <div>
-      <h1>Contact us form</h1>
-    </div>
-  )
+    
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h1>Contact us</h1>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea type="text" name="name" />
+        </div>
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+    
+  );
 }
