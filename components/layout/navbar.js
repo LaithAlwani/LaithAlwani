91c915e4da161f724a688dc0a76548d1aca589d3
Laith Alwani <laithalwani@gmail.com>
2022-01-12 20:20:47 -0500

    added fake logo

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,30 +1,43 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {useRouter} from "next/router"
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const router = useRouter();
-  const pathname = router.pathname
+  const pathname = router.pathname;
 
   return (
     <nav>
       <div className="logo">
         <Link href="/">
-         {/* <h3 className="logo">LA</h3>  */}
-        <a>
-        <Image src="/images/my-image.png" width={64} height={64} alt="logo" />
-         </a>
-      </Link> 
+          {/* <span className="logo-icon">LA</span>  */}
+          <a>
+            <Image
+              src="/images/logo.png"
+              width={64}
+              height={64}
+              alt="logo"
+            />
+          </a>
+        </Link>
       </div>
       <Link href="/">
-        <a className={pathname === "/"? "btn link active" :"btn link"}>About</a>
+        <a className={pathname === "/" ? "btn link active" : "btn link"}>
+          About
+        </a>
       </Link>
       <Link href="/portfolio">
-        <a className={pathname === "/portfolio"? "btn link active" :"btn link"}>Portfolio</a>
+        <a
+          className={pathname === "/portfolio" ? "btn link active" : "btn link"}
+        >
+          Portfolio
+        </a>
       </Link>
       <Link href="/contact">
-        <a className={pathname === "/contact"? "btn link active" :"btn link"}>Contact</a>
+        <a className={pathname === "/contact" ? "btn link active" : "btn link"}>
+          Contact
+        </a>
       </Link>
     </nav>
   );
