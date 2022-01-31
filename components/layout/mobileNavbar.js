2802e947ff7ec186faae14a97ef48dbd91f2e6e4
Laith Alwani <laithalwani@gmail.com>
2022-01-30 21:19:02 -0500

    updated home page text

---
+++ components/layout/mobileNavbar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MobileNavbar() {
+  return <div><h3>Mobile Navbar</h3></div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -3,39 +3,42 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className={styles.about}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/images/my-image.png"
+          alt="Laith Alwani"
+          id="my-image"
+          priority
+          width={900}
+          height={800}
+        />
+      </div>
 
-      <div className={styles.about}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="/images/my-image.png"
-            alt="Laith Alwani"
-            id="my-image"
-            priority
-            width={900}
-            height={800}
-          />
-        </div>
-
-        <div>
-          <h1>&#60;About Me&#62;</h1>
-          <p>
-            Full stack web developer with a background in economics and HR
-            3+ years of experience in web development and a certificate in full stack
-            development from Carleton University. I have experiance in Next.js
-            React.js, JavaScript, HTML, CSS, MySQL, MongoDB and responsive web
-            design.
-          </p>
-          <a
-            href="./resume.pdf"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            Resume
-          </a>
-        </div>
+      <div>
+        <h1>&#60;About Me&#62;</h1>
+        <p>
+          Full stack web developer with Economics and HR backgrounds, Recently
+          earned a Certificate in Full Stack Development from Carleton
+          University, with newly developed skills in, React.js, JavaScript,
+          HTML, CSS, MySQL, MongoDB and responsive web design.
+        </p>
+        <p>
+          Strengths in analyzing problems and building solutions, passionate
+          about developing web applications, with a focus on mobile-first design
+          and development. With each project, my aim is to Automate processes
+          and use my skills to solve problems efficiently.
+        </p>
+        <p>
+          I Led a team of three to develop a single-page MERN app that helps
+          team managers track their players, workouts, schedule and attendance
+          to special events. I’m excited to leverage my skills as part of a
+          fast-paced, quality-driven team to build better experiences on the
+          web.
+        </p>
+        <a href="./resume.pdf" target="_blank" rel="noreferrer" className="btn">
+          Resume
+        </a>
       </div>
     </div>
   );
