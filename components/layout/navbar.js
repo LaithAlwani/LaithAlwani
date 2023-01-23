04232bfd85ec24e0270ac2adb1c621f8e69cd3f1
Laith Alwani <laithalwani@gmail.com>
2023-01-22 21:54:33 -0500

    adds mobile nav

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,34 +1,46 @@
-import {useState} from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Navbar() {
-  const [hash, setHash] = useState("")
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <nav>
-      <div className="logo">
-        <Link href="/">
-          <Image
-            src="/images/my-image.png"
-            alt="Laith Alwani"
-            id="avatar"
-            priority
-            width={48}
-            height={48}
-          />
-          
-        </Link>
-        <div style={{ marginLeft: "0.5rem" }}>
-          <h2 style={{ margin: 0 }}>Laith Alwani</h2>
-          <em style={{ fontSize: "0.8rem" }}>Full Stack Developer</em>
+    <>
+      <nav>
+        <div className="logo">
+          <Link href="/">
+            <Image
+              src="/images/my-image.png"
+              alt="Laith Alwani"
+              id="avatar"
+              priority
+              width={48}
+              height={48}
+            />
+          </Link>
+          <div style={{ marginLeft: "0.5rem" }}>
+            <h2 style={{ margin: 0 }}>Laith Alwani</h2>
+            <em style={{ fontSize: "0.8rem" }}>Full Stack Developer</em>
+          </div>
         </div>
-      </div>
-      <div className="nav-links">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
-      </div>
-    </nav>
+        <div className="nav-links">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+        <button onClick={() => setToggleMenu(!toggleMenu)} className="mobile-nav-button">
+          X
+        </button>
+      </nav>
+      {toggleMenu && (
+        <div className="nav-mobile" onClick={()=>setToggleMenu(!toggleMenu)}>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+      )}
+    </>
   );
 }
