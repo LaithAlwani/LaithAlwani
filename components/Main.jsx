36b3b8f12c6d097836236af9a0233dfc7d4ed28d
Laith Alwani <laithalwani@gmail.com>
2023-01-25 12:15:59 -0500

    fixes social links

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,10 +5,10 @@ export default function Main() {
   return (
     <section className="section" id="home">
       {/* <Image src="/images/green-bg.svg" alt="" fill objectFit="cover" priority /> */}
-      <div className="main">
         <ul className="social-links-main">
           {socialLinks.map((link, i)=>(<li key={i}><SocialLink link={link}/></li>))}
         </ul>
+      <div className="main">
         <h1>Hello, I&apos;m Laith Alwani</h1>
         <p>
           A Full Stack Developer with 3+ years experience with a background in Economics and Human Resources.
