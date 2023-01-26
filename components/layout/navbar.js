53dd8aadd9c8c4825f3cf1f4bd86f9b303afe926
Laith Alwani <laithalwani@gmail.com>
2023-01-26 14:21:12 -0500

    adds comments and credits to svgbackgrounds.com

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -12,6 +12,7 @@ export default function Navbar() {
         setToggleMenu(false);
       }
     });
+    //remove listner
     return () => window.removeEventListener("resize", () => null);
   }, []);
 
