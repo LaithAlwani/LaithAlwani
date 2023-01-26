7842927dc52e392b1e6ac8e027d40ce05f45def5
Laith Alwani <laithalwani@gmail.com>
2023-01-25 19:28:51 -0500

    form auto complete set to false, removes boarder from input fields

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -58,7 +58,7 @@ export default function Contact() {
         header="contact"
         subHeader="Feel free to contact me by filling out the form below and I will get back to you as soon as I can"
       />
-        <form onSubmit={handleSubmit} autoComplete="false">
+        <form onSubmit={handleSubmit} autocomplete="off">
           {error && <div className="message error">{error}</div>}
           {success && <div className="message success">{success}</div>}
           <label htmlFor="name">Name</label>
