825c76fba3b5aba1e526e6da322caaf48fb588de
laith <laithalwani@gmail.com>
2023-11-19 09:36:01 -0500

    fixes contact form issues

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Header from "../../components/Header";
 
@@ -6,61 +6,60 @@ export default function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
   const [success, setSuccess] = useState("");
 
   const handleChange = (e, field) => {
-    if (field === "name") setName(e.target.value);
-    else if (field === "email") setEmail(e.target.value);
-    else setMessage(e.target.value);
+    if (field === "name") {
+      setName(e.target.value)
+      setNameError(false);
+    }
+    if (field === "email") {
+      setEmail(e.target.value)
+      setEmailError(false)
+    };
+    if (field === "message") {
+      setMessage(e.target.value)
+      setMessageError(false);
+    };
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (!name) {
-      setError("please enter your name");
-      return setTimeout(() => {
-        setError("");
-      }, 2000);
+      setNameError(true);
     }
     if (!email) {
-      setError("please enter your email");
-      return setTimeout(() => {
-        setError("");
-      }, 2000);
+      setEmailError(true);
     }
     if (!message) {
-      setError("please enter a message");
-      return setTimeout(() => {
-        setError("");
-      }, 2000);
+      setMessageError(true);
     }
-
     if (message.length > 250) {
-      setError("message is longer than 250 characters");
-      return setTimeout(() => {
-        setError("");
+      setMessageError(true);
+    }
+    if (name && email && message && message.length <= 250) {
+      setName("");
+      setEmail("");
+      setMessage("");
+      setSuccess("Request Sent! Thank you");
+      setTimeout(() => {
+        setSuccess("");
       }, 2000);
     }
-
-    setName("");
-    setEmail("");
-    setMessage("");
-    setSuccess("Request Sent! Thank you");
-    setTimeout(() => {
-      setSuccess("");
-    }, 2000);
   };
+
   return (
     <section className="section" id="contact">
       <div className="container">
-      <Header
-        header="contact"
-        subHeader="Feel free to contact me by filling out the form below and I will get back to you as soon as I can"
-      />
+        <Header
+          header="contact"
+          subHeader="Feel free to contact me by filling out the form below and I will get back to you as soon as I can"
+        />
         <form onSubmit={handleSubmit} autoComplete="off">
-          {error && <div className="message error">{error}</div>}
           {success && <div className="message success">{success}</div>}
           <label htmlFor="name">Name</label>
           <input
@@ -69,15 +68,17 @@ export default function Contact() {
             placeholder="John"
             value={name}
             onChange={(e) => handleChange(e, "name")}
+            className={nameError ? "input-error" : ""}
           />
 
           <label htmlFor="email">Email</label>
           <input
-            type="text"
+            type="email"
             name="email"
             placeholder="johnsmith@example.com"
             value={email}
             onChange={(e) => handleChange(e, "email")}
+            className={emailError ? "input-error" : ""}
           />
           <label htmlFor="message">Message</label>
           <textarea
@@ -86,9 +87,10 @@ export default function Contact() {
             placeholder="Message should 250 characters or less..."
             value={message}
             onChange={(e) => handleChange(e, "message")}
+            className={messageError ? "input-error" : ""}
           />
           <span className={message.length >= 251 ? "message-length error" : "message-length"}>
-            {250 - message.length} {" "}
+            {250 - message.length}{" "}
             {message.length < 249 || message.length > 251 ? "charcters" : "charcter"} remaning
           </span>
 
