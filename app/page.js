d430939a9c70137f9bd0b8d374aee2a9ba16044d
laith <laithalwani@gmail.com>
2023-12-16 18:17:59 -0500

    fixes social links on desktop duplication issue

--- app/page.js
+++ app/page.js
@@ -1,20 +1,22 @@
-import socialLinks from "../utils/SocialLinksList"
-import SocialLink from '../components/ui/SocialLink'
+import socialLinks from "../utils/SocialLinksList";
+import SocialLink from "../components/ui/SocialLink";
 
-export default function Home() { 
+export default function Home() {
   return (
     <section className="section" id="home">
-      {/* <Image src="/images/green-bg.svg" alt="" fill objectFit="cover" priority /> */}
-        <ul className="social-links-main">
-          {socialLinks.map((link, i)=>(<li key={i}><SocialLink link={link}/></li>))}
-        </ul>
       <div className="main">
         <h1>Hello, I&apos;m Laith Alwani</h1>
         <p>
-          A Full Stack Developer with 4+ years experience with a background in Economics and Human Resources.
+          A Full Stack Developer with 4+ years experience with a background in Economics and Human
+          Resources.
         </p>
-        <a href="https://docs.google.com/document/d/1B6WerX9wRwKTOYkq_GHP3U0oDAGpZdTT/edit?usp=sharing&ouid=117539619851358162805&rtpof=true&sd=true" className="btn p-3" target="_blank">Resume</a>
+        <a
+          href="https://docs.google.com/document/d/1B6WerX9wRwKTOYkq_GHP3U0oDAGpZdTT/edit?usp=sharing&ouid=117539619851358162805&rtpof=true&sd=true"
+          className="btn p-3"
+          target="_blank">
+          Resume
+        </a>
       </div>
     </section>
-  )
+  );
 }
